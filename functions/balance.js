ab8a85bde40f549027d861192e11e0c54d8a78a8
joao37226641-dotcom <joao37226641@gmail.com>
2025-10-22 09:28:35 -0300

    balance.js

--- functions/balance.js
+++ functions/balance.js
@@ -1,6 +1,35 @@
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+
 export async function handler(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Netlify Function funcionando!" }),
-  };
+  try {
+    const { wallet } = JSON.parse(event.body);
+
+    if (!wallet) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Carteira não enviada" })
+      };
+    }
+
+    const publicKey = new PublicKey(wallet);
+    const connectionMainnet = new Connection(clusterApiUrl("mainnet-beta"));
+    const connectionTestnet = new Connection(clusterApiUrl("testnet"));
+
+    const balanceMain = await connectionMainnet.getBalance(publicKey);
+    const balanceTest = await connectionTestnet.getBalance(publicKey);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        mainnet: balanceMain / 1e9,
+        testnet: balanceTest / 1e9
+      })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
 }
